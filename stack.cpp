3ed999b6e348698155a45ad6417b22d28a77a2ac
AkhmadSaputraPLBN <c030324006@mahasiswa.poliban.ac.id>
2025-06-02 19:06:45 +0800

    selesai deh

--- stack.cpp
+++ stack.cpp
@@ -26,6 +26,7 @@ void pop(Stack *S, ItemType *x) {
         cout << "Stack kosong!" << endl;
     } else {
         --(S->Count);
+           cout << "Ambil dong:" << S->Item[S->Count] << endl;
               *x = S->Item[S->Count];
     }
 }
@@ -35,6 +36,31 @@ void push(ItemType x, Stack *S){
         cout << "Stack penuh!" << endl;
     } else {
         S->Item[S->Count] = x;
+            cout << "Tambah dong:" << S->Item[S->Count] << endl;
      ++(S->Count);
     }
+}
+int main(){
+    Stack akhs;
+    awal(&akhs);
+
+    ItemType *hehe = akhs.Item;
+    pop(&akhs, hehe);
+    push(99, &akhs);
+    push(73, &akhs);
+    push(23, &akhs);
+    push(88, &akhs);
+    pop(&akhs, hehe);
+    push(12, &akhs);
+    push(65, &akhs);
+    push(53, &akhs);
+    push(45, &akhs);
+    push(34, &akhs);
+    push(78, &akhs);
+    push(90, &akhs);
+    pop(&akhs, hehe);
+    push(100, &akhs);
+    push(101, &akhs);
+    push(102, &akhs);
+    return 0;
 }
